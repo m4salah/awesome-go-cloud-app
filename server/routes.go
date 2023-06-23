edf45e32c0d658b6fd901828e318d08c17d7bf58
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-06-23 20:37:22 +0300

    mock signupper struct to test the endpoint

--- server/routes.go
+++ server/routes.go
@@ -3,14 +3,24 @@ package server
 import (
 	"canvas/handlers"
 	"canvas/k"
+	"canvas/model"
+	"context"
 
 	"github.com/go-chi/chi/v5"
 )
+
+type signupperMock struct{}
+
+func (s signupperMock) SignupForNewsletter(ctx context.Context, email model.Email) (string, error) {
+	return "", nil
+}
 
 func (s *Server) setupRoutes() {
 	s.mux.Route(k.GlobalPrefix, func(r chi.Router) {
 		handlers.Health(r)
 		handlers.Homepage(r)
 		handlers.FrontPage(r)
+		handlers.NewsletterSignup(r, &signupperMock{})
+		handlers.NewsletterThanks(r)
 	})
 }
