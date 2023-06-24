59caf6fbf0aedf45fd8ed55b378da71b78dae8dd
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-06-24 16:39:01 +0300

    save the state of the terraform in s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,59 @@ terraform {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "canvas-terraform-up-and-running-state"
+    key            = "staging-state"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-up-and-running-lock"
+    encrypt        = "true"
+    profile        = "MohamedASalah"
+  }
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "canvas-terraform-up-and-running-state"
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.terraform_state.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.terraform_state.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-up-and-running-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 provider "aws" {
   region                   = "eu-central-1"
   shared_credentials_files = ["~/.aws/credentials"]
