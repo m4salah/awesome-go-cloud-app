642f56dd03c082027ad6b54fccac70ffcb305a20
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-07-14 15:36:14 +0200

    remove token as range key

--- terraform/main.tf
+++ terraform/main.tf
@@ -197,17 +197,11 @@ resource "aws_dynamodb_table" "newsletter_subscribers" {
   name             = "newsletter_subscribers"
   billing_mode     = "PAY_PER_REQUEST"
   hash_key         = "email"
-  range_key        = "token"
   stream_enabled   = true
   stream_view_type = "NEW_AND_OLD_IMAGES"
 
   attribute {
     name = "email"
-    type = "S"
-  }
-
-  attribute {
-    name = "token"
     type = "S"
   }
 }
