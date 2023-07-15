bd0bb4038e0d0dbf9a3bd0587207aa2a7bc472bb
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2023-07-15 23:40:05 +0200

    mock pinger

--- handlers/health.go
+++ handlers/health.go
@@ -1,14 +1,23 @@
 package handlers
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func Health(mux chi.Router) {
+type pinger interface {
+	Ping(ctx context.Context) error
+}
+
+func Health(mux chi.Router, p pinger) {
 	mux.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		if err := p.Ping(r.Context()); err != nil {
+			http.Error(w, err.Error(), http.StatusBadGateway)
+			return
+		}
 		fmt.Fprintf(w, "Healthy!")
 	})
 }
--- server/routes.go
+++ server/routes.go
@@ -9,7 +9,7 @@ import (
 
 func (s *Server) setupRoutes() {
 	s.mux.Route(k.GlobalPrefix, func(r chi.Router) {
-		handlers.Health(r)
+		handlers.Health(r, s.database)
 		handlers.Homepage(r)
 		handlers.FrontPage(r)
 		handlers.NewsletterSignup(r, s.database)
